5eadce37511f3c12bad5d2fe11a051c5dec4dd0b
Tamo <tamuna.kakhidze.1@btu.edu.ge>
2024-11-12 15:46:20 +0400

    Initial Commit

---
+++ QuizApp/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  QuizApp
+//
+//  Created by Tamuna Kakhidze on 12.11.24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        
+        let viewController = LoginViewController()
+
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ QuizApp/Scenes/LoginPage/ViewController/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Tamuna Kakhidze on 12.11.24.
+//
+
+import UIKit
+
+// MARK: - LoginViewController
+class LoginViewController: UIViewController {
+
+    // MARK: - UI Components
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+    
+    
+}
+
